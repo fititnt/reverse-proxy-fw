589b0c8b72df8320976da22229d7e9b4f0633396
Emerson Rocha Luiz <emerson@alligo.com.br>
2017-03-21 02:32:28 -0300

    Instalado VM, NGinx; adicionado arquivo padrão nginx.conf sem edicoes

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # reverse-proxy-fw
-Cloudflare de pobre. Ou gente com pressa
+
+Cloudflare de pobre. Ou gente com pressa. 
+Veja [vm-setup.sh](vm-setup.sh).
+
+<!--
+## Crie uma conta na Amazon EC2
+Vá para https://aws.amazon.com/pt/resources/create-account/ e crie uma conta.
+Primeira vez? Ótimo, pois terá servidor de graça por um ano. Essa é a ideia.
+
+-->
+
+
+
+
+
---
+++ vm-setup.sh
@@ -0,0 +1,28 @@
+##ubuntu@ip-172-31-14-179:~$ cat /etc/*release
+#DISTRIB_ID=Ubuntu
+#DISTRIB_RELEASE=16.04
+#DISTRIB_CODENAME=xenial
+#DISTRIB_DESCRIPTION="Ubuntu 16.04.2 LTS"
+#NAME="Ubuntu"
+#VERSION="16.04.2 LTS (Xenial Xerus)"
+#ID=ubuntu
+#ID_LIKE=debian
+#PRETTY_NAME="Ubuntu 16.04.2 LTS"
+#VERSION_ID="16.04"
+#HOME_URL="http://www.ubuntu.com/"
+#SUPPORT_URL="http://help.ubuntu.com/"
+#BUG_REPORT_URL="http://bugs.launchpad.net/ubuntu/"
+#VERSION_CODENAME=xenial
+
+
+
+sudo apt-get update
+sudo apt-get install nginx -y
+
+# Usando firewall da EC2
+
+systemctl status nginx
+systemctl start nginx
+
+# WinSCP as Sudo https://winscp.net/eng/docs/faq_su
+sudo sh -c 'echo "ubuntu  ALL=NOPASSWD: /bin/sftp-server" >> /etc/sudoers'
